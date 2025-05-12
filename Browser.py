a538ca6ab3cc30f0070d283a721855d21f88a80a
Lukas Mundt <mail@lukas-mundt.de>
2025-05-12 20:08:19 +0200

    3.7 Font Caching

--- Browser.py
+++ Browser.py
@@ -5,6 +5,7 @@ from typing import Literal
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 SCROLL_STEP = 100
+FONTS = {}
 
 class Text:
     def __init__(self, text):
@@ -71,6 +72,16 @@ def lex(body):
         out.append(Text(buffer))
     return out
 
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight,
+                                 slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
+
 class Layout:
     def __init__(self, tokens):
         self.line = []
@@ -111,11 +122,7 @@ class Layout:
             self.cursor_y += VSTEP
 
     def word(self, word):
-        font = tkinter.font.Font(
-            size=self.size,
-            weight=self.weight,
-            slant=self.style,
-        )
+        font = get_font(self.size, self.weight, self.style)
         w = font.measure(word)
         if self.cursor_x + w > WIDTH - HSTEP:
             self.flush()
