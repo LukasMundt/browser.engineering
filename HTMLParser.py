3f98d8bc88d797fe258bf1e6272eb6c85a96bc3a
Lukas Mundt <mail@lukas-mundt.de>
2025-05-22 14:23:23 +0200

    4.2 Constructing the Tree

--- HTMLParser.py
+++ HTMLParser.py
@@ -1,3 +1,6 @@
+from Browser import Text, Element
+
+
 class HTMLParser:
     def __init__(self, body):
         self.body = body
@@ -21,3 +24,27 @@ class HTMLParser:
         if not in_tag and text:
             out.append(Text(text))
         return out
+
+    def add_text(self, text):
+        parent = self.unfinished[-1]
+        node = Text(text, parent)
+        parent.children.append(node)
+
+    def add_tag(self, tag):
+        if tag.startswith("/"):
+            if len(self.unfinished) == 1: return # For the last node
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        else:
+            parent = self.unfinished[-1] if self.unfinished else None # For the first node (has no parent)
+            parent = self.unfinished[-1]
+            node = Element(tag, parent)
+            self.unfinished.append(node)
+
+    def finish(self):
+        while len(self.unfinished) > 1:
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        return self.unfinished.pop()
