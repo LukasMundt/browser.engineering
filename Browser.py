4c11a539b3f855e2764b34ebf5ffbb439aaf0531
Lukas Mundt <mail@lukas-mundt.de>
2025-06-02 20:24:00 +0200

    6.6 Inherited styles

--- Browser.py
+++ Browser.py
@@ -19,6 +19,13 @@ BLOCK_ELEMENTS = [
     "legend", "details", "summary"
 ]
 
+INHERITED_PROPERTIES = {
+    "font-size": "16px",
+    "font-style": "normal",
+    "font-weight": "normal",
+    "color": "black",
+}
+
 DEFAULT_STYLE_SHEET = CSSParser(open("browser.css").read()).parse()
 
 class Browser:
@@ -82,6 +89,11 @@ class Browser:
 
 def style(node, rules):
     node.style = {}
+    for property, default_value in INHERITED_PROPERTIES.items():
+        if node.parent:
+            node.style[property] = node.parent.style[property]
+        else:
+            node.style[property] = default_value
     for selector, body in rules:
         if not selector.matches(node): continue
         for property, value in body.items():
@@ -90,6 +102,14 @@ def style(node, rules):
         pairs = CSSParser(node.attributes["style"]).body()
         for property, value in pairs.items():
             node.style[property] = value
+    if node.style["font-size"].endswith("%"):
+        if node.parent:
+            parent_font_size = node.parent.style["font-size"]
+        else:
+            parent_font_size = INHERITED_PROPERTIES["font-size"]
+        node_pct = float(node.style["font-size"][:-1]) / 100
+        parent_px = float(parent_font_size[:-2])
+        node.style["font-size"] = str(node_pct * parent_px) + "px"
     for child in node.children:
         style(child, rules)
 
