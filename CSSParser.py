69a26e2737853dd71066117ab40c2f3e4baa017c
Lukas Mundt <mail@lukas-mundt.de>
2025-06-02 15:42:35 +0200

    6.3 Selectors

--- CSSParser.py
+++ CSSParser.py
@@ -1,3 +1,6 @@
+from HTMLParser import Element
+
+
 class CSSParser:
     def __init__(self, s):
         self.s = s
@@ -33,7 +36,7 @@ class CSSParser:
 
     def body(self):
         pairs = {}
-        while self.i < len(self.s):
+        while self.i < len(self.s) and self.s[self.i] != "}":
             try:
                 prop, val = self.pair()
                 pairs[prop.casefold()] = val
@@ -41,7 +44,7 @@ class CSSParser:
                 self.literal(";")
                 self.whitespace()
             except Exception:
-                why = self.ignore_until([";"])
+                why = self.ignore_until([";", "}"])
                 if why == ";":
                     self.literal(";")
                     self.whitespace()
@@ -56,3 +59,53 @@ class CSSParser:
             else:
                 self.i += 1
         return None
+
+    def selector(self):
+        out = TagSelector(self.word().casefold())
+        self.whitespace()
+        while self.i < len(self.s) and self.s[self.i] != "{":
+            tag = self.word()
+            descendant = TagSelector(tag.casefold())
+            out = DescendantSelector(out, descendant)
+            self.whitespace()
+        return out
+
+def parse(self):
+    rules = []
+    while self.i < len(self.s):
+        try:
+            self.whitespace()
+            selector = self.selector()
+            self.literal("{")
+            self.whitespace()
+            body = self.body()
+            self.literal("}")
+            rules.append((selector, body))
+        except Exception:
+            why = self.ignore_until(["}"])
+            if why == "}":
+                self.literal("}")
+                self.whitespace()
+            else:
+                break
+    return rules
+
+class TagSelector:
+    def __init__(self, tag):
+        self.tag = tag
+
+    def matches(self, node):
+        return isinstance(node, Element) and self.tag == node.tag
+
+
+class DescendantSelector:
+    def __init__(self, ancestor, descendant):
+        self.ancestor = ancestor
+        self.descendant = descendant
+
+    def matches(self, node):
+        if not self.descendant.matches(node): return False
+        while node.parent:
+            if self.ancestor.matches(node.parent): return True
+            node = node.parent
+        return False
